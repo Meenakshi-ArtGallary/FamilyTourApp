e90a1961ece28817134dac46d5f9cca91443efaa
meenakshi sundari <meenmalar@gmail.com>
2025-05-18 20:34:56 +0530

    Footer lable change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 function Footer() {
   return (
     <footer className="footer">
-      <h1 className='footer-text'>&copy; 2025 Travel Adventures. All rights reserved.</h1>
+      <h1 className='footer-text'>&copy; 2025 Family Tour App. All rights reserved.</h1>
     </footer>
   );
 }
